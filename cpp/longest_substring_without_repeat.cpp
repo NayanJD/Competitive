1141be321db19e7bd7379ef1ff0e773b89fd06bd
Nayan Das <nayan.jyoti@olx.com>
2023-03-26 21:20:17 +0530

    Modify longest substring. Add ccls

--- cpp/longest_substring_without_repeat.cpp
+++ cpp/longest_substring_without_repeat.cpp
@@ -1,37 +1,81 @@
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+// Given a string s, find the length of the longest 
+// substring
+//  without repeating characters.
+// 
+//  
+// 
+// Example 1:
+// 
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+// Example 2:
+// 
+// Input: s = "bbbbb"
+// Output: 1
+// Explanation: The answer is "b", with the length of 1.
+// Example 3:
+// 
+// Input: s = "pwwkew"
+// Output: 3
+// Explanation: The answer is "wke", with the length of 3.
+// Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+//  
+// 
+// Constraints:
+// 
+// 0 <= s.length <= 5 * 104
+// s consists of English letters, digits, symbols and spaces.
+
 #include <iostream>
 #include <unordered_map>
+#include <algorithm>
 
 using namespace std;
+
+
+void showLetters(unordered_map<char, int> letters) {
+    for(auto letter: letters) {
+        printf("%c ", letter.first);
+    }
+}
 
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        auto i = s.begin();
-        auto j = s.begin();
 
-        int maxLength = 0;
-        unordered_map<char, int> isPresentMap;
+        int n = s.length();
+        int startingPoint = 0;
+        int result = 0;
 
-        while(i != s.end()){
-            if(isPresentMap.find(*i) != isPresentMap.end()) {
-                while(j != i) {
-                    if (*i == *j){
-                        j++;
-                        break;
-                    }
-                    isPresentMap.erase(*j);
-                    j++;
+        unordered_map<char, int> letters;
+
+        for(int i = 0; i < n; i++) {
+            if(letters.find(s[i]) == letters.end()) {
+                letters[s[i]] = i;
+                
+            } else {
+                int idx = letters[s[i]];
+
+                while(startingPoint <= idx) {
+                    letters.erase(s[startingPoint]);
+                    startingPoint++;
                 }
+
+                letters[s[i]]= i;
             }
             
-            isPresentMap[*i] = 1;
+            if(result < letters.size()) {
+                result = letters.size();
+            }
 
-            maxLength = max(maxLength, (int) isPresentMap.size());
-            i++;
+            //printf("i: %d, letters: ", i);
+            //showLetters(letters);
+            //printf("\n");
         }
 
-        return maxLength;
-
+        return result;
     }
 };
 
